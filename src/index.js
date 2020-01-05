a2e90fe518b3a695d58b692af9e1ebc0c4928133
Joshua <psyjr4@nottingham.ac.uk>
2020-01-05 11:42:29 +0000

    Changed static setup

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ var categories = initialCategories;
 
 var answer;
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
 function displayScoreBoard() {
