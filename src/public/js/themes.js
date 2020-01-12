75ed1a46a37d9776a798734f8eb64bb93b145239
Joshua <psyjr4@nottingham.ac.uk>
2020-01-12 00:41:50 +0000

    Fixed another theme issue

--- src/public/js/themes.js
+++ src/public/js/themes.js
@@ -29,7 +29,7 @@ function themeSwitcher(theme) {
             blackTheme();
             break;
         default:
-            defaultTheme();
+            blackTheme();
             break;
     }
 }
